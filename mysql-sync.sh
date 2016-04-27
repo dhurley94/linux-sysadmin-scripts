f5323aa69ea2bb6978a5fb942188c6a34f82ffb9
dhurley94 <dhurley@singlehop.com>
2016-04-27 12:57:57 -0400

    Update mysql-sync.sh

--- mysql-sync.sh
+++ mysql-sync.sh
@@ -1,11 +1,69 @@
 #!/bin/bash
 
-function mysql()
+function mysqldump_local()
 {
-	mysqldump > /root/dstgrab.sql
+	# local mysql loop
+	while true; do
+		# create dst dump
+		mysqldump > /root/dstgrab.sql
+		# check if exists
+		if [ -e /root/dstgrab.sql ]; then
+			echo "created dst mysql dump"
+			break
+		fi
+	done
 	
-	ssh root@$ip 'mysqldump > /root/srcgrab.sql'
-	echo $pass
-	
-	mysql < /root/srcgrab.sql
+	echo "please verify this completed successfully before continuing."
+	read $wait
 }
+function mysqldump_remote()
+{
+	# remote mysql loop
+	while true; do
+		# create src dump
+		ssh root@$ip:$port 'mysqldump > /root/srcgrab.sql'
+		echo $pass
+
+	# download src dump
+		rsync aux -e 'ssh -p '$port root@$ip:/root/srcgrab.sql /root/
+		echo $pass
+		
+	# check if exists
+		if [ -e "/root/srcgrab.sql" ]; then
+			echo "succeeded in downloading src mysql dump"
+			break
+		else
+			echo "failed in downloading the sql dump. retrying..."
+		fi
+	done
+	
+	echo "please verify this completed successfully before continuing."
+	read $wait
+}
+function mysqlimport()
+{
+	if [ -e "/root/srcgrab.sql" ]; then
+		mysql < srcgrab.sql
+	else
+		echo "import failed. check error logs before continuing."
+		read $wait
+	fi
+}
+while true; do
+        echo "input SRC ip address"
+        read $ip
+
+        echo "input root password"
+        read $pass
+
+        echo "input port number"
+        read $port
+
+        echo "please press enter to begin process"
+        read $wait
+
+        mysqldump_local
+		mysqldump_remote
+        mysqlimport
+        echo "process has completed. please verify" >> /root/mysqlsync.log
+done
