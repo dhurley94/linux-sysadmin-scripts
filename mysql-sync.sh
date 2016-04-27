40a96a8507f02108127e6bbb4917717ccc73159b
dhurley94 <dhurley@singlehop.com>
2016-04-27 13:44:09 -0400

    Update mysql-sync.sh

--- mysql-sync.sh
+++ mysql-sync.sh
@@ -2,76 +2,76 @@
 
 function mysqldumplocal()
 {
-	# local mysql loop
-	while true; do
-		# create dst dump
-		mysqldump > /root/dstgrab.sql
-		# check if exists
-		if [ -e /root/dstgrab.sql ]; then
-			echo "Destination MySQL dump has been created."
-			break
-		fi
-	done
-	
-	echo "Press enter to continue."
-	read $wait
+        # local mysql loop
+        while true; do
+                # create dst dump
+                mysqldump > /root/dstgrab.sql
+                # check if exists
+                if [ -e /root/dstgrab.sql ]; then
+                        echo "Destination MySQL dump has been created."
+                        break
+                fi
+        done
+
+        echo "Press enter to continue."
+        read wait
 }
 
 function mysqldumpremote()
 {
-	# remote mysql loop
-	while true; do
-		# create src dump
-		ssh root@$ip:$port 'mysqldump > /root/srcgrab.sql'
-		echo $pass
+        # remote mysql loop
+        while true; do
+                # create src dump
+                ssh root@$ip:$port "mysqldump > /root/srcgrab.sql"
+                echo $pass
 
-		# download src dump
-		rsync aux -e 'ssh -p '$port root@$ip:/root/srcgrab.sql /root/
-		echo $pass
-		
-		# check if exists
-		if [ -e "/root/srcgrab.sql" ]; then
-			echo "Creation of MySQL dump of source server was successful."
-			break
-		else
-			echo "Failed in downloading the sql dump. Enter to retry."
-			read $wait
-		fi
-	done
-	
-	echo "Press enter to continue."
-	read $wait
+                # download src dump
+                rsync aux -e 'ssh -p "$port"' root@"$ip":/root/srcgrab.sql /root/
+                echo $pass
+
+                # check if exists
+                if [ -e "/root/srcgrab.sql" ]; then
+                        echo "Creation of MySQL dump of source server was successful."
+                        break
+                else
+                        echo "Failed in downloading the sql dump. Enter to retry."
+                        read $wait
+                fi
+        done
+
+        echo "Press enter to continue."
+        read $wait
 }
 
 function mysqlimport()
 {
-	if [ -e "/root/srcgrab.sql" ]; then
-		echo "Beginning import of source SQL dump."
-		mysql < srcgrab.sql
-		echo "Process has completed. Please test and verify nothing broke."
-		read $wait
-	fi
+        if [ -e "/root/srcgrab.sql" ]; then
+                echo "Beginning import of source SQL dump."
+                mysql < srcgrab.sql
+                echo "Process has completed. Please test and verify nothing broke."
+                read $wait
+        fi
 }
 
 while true; do
         echo "Input source server's ip address."
-        read $ip
+        read ip
 
         echo "Input source servers root password."
-        read -s $pass
+        read -s pass
 
-        echo "Input source server's SSH port."
-        read $port
-	
-	#if [ $port == "" ]; then
-	#	port=22
-	#fi
-	
+        echo "Input source server's SSH port. Press enter for default."
+        read port
+
+        if [ "$port" = "" ]; then
+                port=22
+        fi
+
         echo "Press enter to continue."
-        read $wait
+        read wait
 
         mysqldumplocal
-	mysqldumpremote
-        mysqlimport
+                mysqldumpremote
+
         echo "Process has completed."
 done
