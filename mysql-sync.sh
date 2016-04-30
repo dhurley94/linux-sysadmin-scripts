640088341a4dedead5b5f23451f6a1f0079f9d42
dhurley94 <dhurley@singlehop.com>
2016-04-30 04:39:44 -0400

    Update mysql-sync.sh

--- mysql-sync.sh
+++ mysql-sync.sh
@@ -21,47 +21,25 @@ function mysqldumpremote()
 	if [ ! -e "/root/srcgrab.sql" ]; then 
 		ssh root@$ip -p $port "mysqldump --all-databases > /root/srcgrab.sql"
 		rsync -aux -e "ssh -p $port" root@"$ip":/root/srcgrab.sql /root/
-        # remote mysql loop
-        while true; do
-                # create src dump
-                ssh root@$ip -p $port "mysqldump --all-databases > /root/srcgrab.sql"
-                echo "Created MySQL dump on source."
-
-                # download src dump
-                rsync -aux -e "ssh -p $port" root@"$ip":/root/srcgrab.sql /root/
-                echo "Downloaded dump from source."
-
-                # check if exists
-                if [ -e "/root/srcgrab.sql" ]; then
-                        echo "Download of source MySQL dump of source server was successful."
-                        break
-                else
-                        echo "Failed in downloading the sql dump. Enter to retry."
-                        read wait
-                fi
+			if [ -e "/root/srcgrab.sql" ]; then
+				echo "Download of source MySQL dump of source server was successful."
+            else
+                echo "Failed in downloading the sql dump."
+                read wait
+            fi
         done
 
         echo "Press enter to continue to import of databases."
         read wait
 }
-
 function mysqlimport()
 {
         if [ -e "/root/srcgrab.sql" ]; then
-                echo "Beginning import of source SQL dump."
-                mysql < srcgrab.sql
-                echo "Process has completed. Please test and verify nothing broke."
-                read wait
+            echo "Beginning import of source SQL dump."
+            mysql < srcgrab.sql
+            echo "Process has completed. Please test and verify nothing broke."
         fi
 }
-
-# Doesn't work properly. Probably will need to use keys or sshpass
-#function passPass()
-#{
-#	expect "assword:"
-#	send $pass"\r"
-#	interact
-#}
 
 while true; do
         echo "Input source server's ip address."
@@ -74,15 +52,12 @@ while true; do
                 port=22
         fi
 		
-		#echo "Input source servers root password."
-        #read -s pass
-		
-        echo "Press enter to begin destination dump creation."
+        echo "Press enter to begin."
         read wait
 
         mysqldumplocal
         mysqldumpremote
         mysqlimport
-
+	echo "MySQL import completed."
         break
 done
