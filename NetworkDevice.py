ed43b3a7388e89988ed00c62336e6594000752e8
dhurley94 <dhurley@singlehop.com>
2016-05-09 19:59:34 -0400

    Update NetworkDevice.py

--- NetworkDevice.py
+++ NetworkDevice.py
@@ -2,13 +2,14 @@
 from uuid import getnode as get_mac
 
 class NetworkDevice:
-    def __init__(self, dev, mainip, network, gateway, subnet):
+    def __init__(self, dev, mainip, subnet):
         self.dev = dev
         self.mainip = mainip
-        self.network = network
         self.subnet = subnet
-        self.gateway = gateway
-        # uuid > hex > mac conversion
+        self.octets = self.mainip.split('.')
+
+    # local uuid > hex > mac conversion
+    def gethwaddr(self):
         uuid = get_mac()
         uuid = '%012x' % uuid
         mac = ""
@@ -17,47 +18,15 @@ class NetworkDevice:
                 mac += uuid[i] + uuid[i + 1] + ":"
             else:
                 mac += uuid[i] + uuid[i + 1]
-        self.hwaddr = mac.upper()
+        return mac.upper()
 
-        # find available hosts in subnet
+    # return network id
+    def getnetwork(self):
+        return int(self.octets[3]) - 2
 
-        self.sublist = subnet.split('.')
-        self.hosts = 256 - int(self.sublist[3]) - 3
+    # return default gateway
+    def getgateway(self):
+        return int(self.octets[3]) - 1
 
-    # creates ifcfg-eth*
-    def createfile(self, filename):
-            # ie: ifcfg-eth0
-            target = open(filename, 'w+')
-            target.truncate()
-            target.write("ONBOOT=yes\nBOOTPROTO=static\n")
-            target.write("DEVICE=" + self.dev + "\n")
-            target.write("NETWORK=" + ifcfg.network + "\n")
-            target.write("NETMASK=" + ifcfg.subnet + "\n")
-            target.write("IPADDR=" + ifcfg.mainip + "\n")
-            target.write("HWADDR=" + ifcfg.hwaddr + "\n")
-            target.close()
-
-    # creates route-eth*
-    def createroute(self, filename):
-            # ie: route-eth0
-            target = open(filename, 'w+')
-            target.write(self.network + " via " + self.gateway + "\n") # todo needs device and subnet
-            target.close()
-
-    # create /etc/sysconfig/networkyyeee
-    def createnetwork(self):
-        target = open("network", 'w+')
-        target.write("NETWORKING=yes")
-        target.write("GATEWAY=" + self.gateway + "\n")
-        target.close()
-
-    # create array of ip range for config creation
-    def validHosts(self):
-        mainip = self.mainip.split('.')
-        validLast = []
-        for i in range(int(self.hosts)):
-            validLast.append(int(mainip[3]) + i)
-        valid = []
-        for i in range(self.hosts):
-            valid.append(str(mainip[0]) + "." + str(mainip[1])  + "." + str(mainip[2]) + "." + str(validLast[i]))
-        return valid
+    def gethosts(self):
+        return 256 - int(self.octet[3]) - 3
