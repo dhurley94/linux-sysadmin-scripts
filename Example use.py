692c1fe16466a1e5d43a9100074dae159c6962dd
dhurley94 <dhurley@singlehop.com>
2016-05-09 20:18:12 -0400

    Update Example use.py

--- Example use.py
+++ Example use.py
@@ -17,30 +17,28 @@ def createfile(filename, ifcfg):
         target.truncate()
         target.write("ONBOOT=yes\nBOOTPROTO=static\n")
         target.write("DEVICE=" + ifcfg.dev + "\n")
-        target.write("NETWORK=" + ifcfg.network + "\n")
+        target.write("NETWORK=" + ifcfg.getnetwork() + "\n")
         target.write("NETMASK=" + ifcfg.subnet + "\n")
         target.write("IPADDR=" + ifcfg.mainip + "\n")
-        target.write("HWADDR=" + ifcfg.hwaddr + "\n")
+        target.write("HWADDR=" + ifcfg.gethwaddr() + "\n")
         target.close()
-
     # creates route-eth*
 
 # creates route-eth*
 def createroute(filename, ifcfg):
         # ie: route-eth0
         target = open(filename, 'w+')
-        target.write(ifcfg.network + " via " + ifcfg.gateway + "\n") # todo needs device and subnet
+        target.write(ifcfg.getnetwork() + " via " + ifcfg.getgateway() + "\n") # todo needs device and subnet
         target.close()
-
 # create /etc/sysconfig/networkyyeee
 def createnetwork(ifcfg):
     target = open("network", 'w+')
     target.write("NETWORKING=yes")
-    target.write("GATEWAY=" + ifcfg.gateway + "\n")
+    target.write("GATEWAY=" + ifcfg.getgateway()+ "\n")
     target.close()
 
 # create array of ip range for config creation
-def validHosts(mainip, hosts):
+def validHosts(mainip, hosts): # todo fix this
     mainip = mainip.split('.')
     validLast = []
     for i in range(int(hosts)):
@@ -50,9 +48,23 @@ def validHosts(mainip, hosts):
         valid.append(str(mainip[0]) + "." + str(mainip[1])  + "." + str(mainip[2]) + "." + str(validLast[i]))
     return valid
 
-net = NetworkDevice("eth0", "192.168.1.100", "192.168.1.0", "192.168.1.1", "255.255.255.248")
+RANGES = []
 
-print validHosts(net.mainip, net.hosts)
-createfile("testing-eth", net)
-createroute("testing-route", net)
-createnetwork(net)
+num =  int(raw_input("Input how many ranges.\n"))
+
+for i in range(num):
+    print "Data for ip range #" + str(i) + "\n"
+    dev = raw_input("Name of device? ie: eth0\n")
+    mainip = raw_input("First valid ip address\n")
+    subnet = raw_input("Valid Subnet? ie: 255.255.255.248\n\n")
+    RANGES.append(NetworkDevice(dev, mainip, subnet))
+
+for i in range(len(RANGES)):
+    print "Ranges #" + str(i)
+    print "Device ID: " + RANGES[i].dev
+    print "Main IP: " + RANGES[i].mainip
+    print "Subnet: " + RANGES[i].subnet
+    print "Hardware Address: " + RANGES[i].gethwaddr()
+    print "Network ID: " + str(RANGES[i].octets[0]) + "." + str(RANGES[i].octets[1]) + "." + str(RANGES[i].octets[2]) + "." + str(RANGES[i].getnetwork())
+    print "Gateway: " + str(RANGES[i].octets[0]) + "." + str(RANGES[i].octets[1]) + "." + str(RANGES[i].octets[2]) + "." + str(RANGES[i].getgateway())
+    #print "All valid hosts in subnet: " + str(validHosts(RANGES[i].mainip, RANGES[i].gethosts()))
