703a1fd9e55f98b41c53888fbcc901a65dc95da3
dhurley94 <dhurley@singlehop.com>
2016-09-17 17:19:43 -0400

    Update chip.sh

--- chip.sh
+++ chip.sh
@@ -1,57 +1,110 @@
 #!/bin/bash
-# An extremely dirty way to swap network configs between cPanel servers
-# This has been tested on 1 production server so far.
-while true; do
-	### asking for input
-	while true; do
-		echo "Input source server's ip address."
-		read ip
-		echo "Input source server's SSH port. Press enter for default."
-		read port
-		if [ "$port" = "" ]; then
-			port=22
-		fi
-		echo "$ip:$port"
-		echo 'Are both the IP and Port correct? y/n'		
-		read wait
-		if [ "$wait" == "y" ]; then
-			clear
-			break
-		fi
-	done
-	### Create tarballs on source and destination servers
-	### running checks
-	echo "Have the source servers root pass ready."
-	while true; do
-		tar -cvzf /root/network-dst.tar.gz /etc/hosts /etc/ips /etc/sysconfig/network /etc/sysconfig/network-scripts/ifcfg-eth0 /var/cpanel/mainip
-		printf "tar'd networking on dst.\n"
 
-		ssh root@$ip -p $port "tar -cvzf /root/network-src.tar.gz /etc/hosts /etc/ips /etc/sysconfig/network /etc/sysconfig/network-scripts/ifcfg-eth0 /var/cpanel/mainip"	
-		printf "tar'd networking on src.\n"
-		echo 'press enter to continue'
-		read wait
-		clear
-		#echo 'checking if files exist on both servers.'
-		rsync -auv -e "ssh -p $port" root@"$ip":/root/network-src.tar.gz ~
-		printf "downloading tar from src.\n"
-		
-		rsync -auv -e "ssh -p $port" /root/network-dst.tar.gz root@"$ip":~
-		echo 'uploading tar to dst.'
-		if [ -e "/root/network-src.tar.gz" ]; then
-		if [ -e "/root/network-src.tar.gz" ] && [ ssh $ip -p $port "test -e network-dst.tar.gz" ] ; then
-			echo 'it do.'
-			break
-		else
-			echo 'Something failed.'
-			echo 'Please check that both files were created on both servers.';
-			echo $?
-		fi
-	done
-	### let's extract!
-	tar -C / -xf /root/network-src.tar.gz
-	echo 'extracted tarball on dst.'
-	ssh root@$ip -p $port "tar -C / -xf /root/network-dst.tar.gz"
-	echo 'extracted tarball on src.'
-break
+function MENU
+{
+   echo "
+Script to swap IP addresses between two cPanel servers.
+REQUIRED OPTIONS:
+	-s
+      set the source ip address.
+	  
+OPTIONS:
+	
+	-p
+      set the source port, defaults to 22 if no input.
+	
+	-k 
+      are ssh keys in use? set 1 if yes. defaults to ssh key creation
+	
+Example: ./chip.sh -sp 192.168.0.20 2222
+Example: ./chip.sh -sk 192.168.0.20 1
+"
+	exit 1
+}
+
+setup_sshkey() {
+	ssh-keygen -t rsa
+	ssh-copy-id -p $sourceport root@$sourceip
+	echo; echo
+}
+
+tarchk() {
+	if [ -e /root/network-dst.tar.gz ] && [ ssh $sourceip -p $sourceport "test -e network-src.tar.gz" ]; then
+		return true;
+	else
+		return false;
+	fi
+}
+
+revert() {
+	echo 'REVERT FUNC'
+}
+
+ipswap() {
+	echo 'IPSWAP FUNC'
+	if [ $credchk -eq 1 ] && [ tarballchk ]; then
+		sed -i "/$destinationip/c\$sourceip/" $ifcfg
+		sed -i "/$destinationgateway/c\$sourcegateway/" $ifcfg
+	
+		ssh root@$sourceip -p $sourceport "cat /etc/ips" > /etc/ips
+		ssh root@$sourceip -p $sourceport "cat /var/cpanel/mainip" > /var/cpanel/mainip
+		ssh root@$sourceip -p $sourceport "cat /etc/hosts" > /etc/hosts
+		ssh root@$sourceip -p $sourceport "cat /etc/sysconfig/network" > /etc/sysconfig/network
+	
+		scp /etc/sysconfig/network root@$sourceip -p $sourceport:/etc/sysconfig
+	
+		ssh root@$sourceip -p $sourceport "sed -i '/$sourceip/c\$destinationip/' $ifcfg"
+		ssh root@$sourceip -p $sourceport "sed -i '/$sourcegateway/c\$destinationgateway' $ifcfg"
+	fi
+}
+
+while getopts ":a:r:s:" opt; do	
+	case $opt in		
+		s)
+			sourceip=$OPTARG
+			flag="s"
+			;;
+		p)
+			sourceport=$OPTARG
+			flag="p"
+			;;
+		k)
+			sshkey=$OPTARG
+			flag="k"
+			;;
+		h)
+			MENU
+			flag="h"
+			;;
+		\?) echo "invalid option: -$OPTARG"; echo; MENU;;
+		:) echo "option -$OPTARG requires an argument."; echo; MENU;;
+	esac
 done
-printf "\nVerify that these files were replaced on the DST before proceeding.\nManually replace the HW ADDR in /etc/sysconfig/network-scripts/ifcfg-eth0 with the one in ifcfg-eth0.bak\nswap the ip addresses, restart networking on both servers, reapply vlans"
+
+if [[ $# -eq 0 || -z $sourceip ]]; then echo MENU; fi  # check for existence of required var
+if [ -z $sourceport ]; then sourceport=22; fi
+if [ -z $sshkey ]; then setup_sshkey; fi
+
+if [[ `whoami` != "root" ]]
+then
+   echo "You'll to be root."
+   exit 1
+fi
+
+#tar -cvzf /root/network-dst.tar.gz /etc/hosts /etc/ips /etc/sysconfig/network /etc/sysconfig/network-scripts/ifcfg-eth0 /var/cpanel/mainip
+#ssh root@$sourceip -p $sourceport "tar -cvzf /root/network-dst.tar.gz /etc/hosts /etc/ips /etc/sysconfig/network /etc/sysconfig/network-scripts/ifcfg-eth0 /var/cpanel/mainip"
+
+#if [ tarchk ]; then
+#	ifcfg = "/etc/sysconfig/network-scripts/ifcfg-eth0"
+#	info[0] = "IPADDR"
+#	info[1] = "GATEWAY"
+#	info[2] = "NETMASK"
+
+#	src[]
+#	dst[]
+	
+#	for i in 2; do
+#		dst[i] = ssh root@$sourceip -p $sourceport "grep info[i] $ifcfg"
+#		src[i] = grep info[i] $ifcfg
+#	done
+#fi
