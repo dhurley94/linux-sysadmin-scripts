c56dca6fb6fa991d7942d3eb2fc1f60226ced831
Dave <dhurley@singlehop.com>
2016-09-30 17:29:51 -0400

    Update fixips.py

--- fixips.py
+++ fixips.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
-# untested
-import os
 import subprocess
 main():
-	if os.path.exists('/etc/redhat-release'):
-		try:
-			mainip=open('/var/cpanel/mainuip', 'r')
-			with open('/etc/trueuserdomain') as input_file:
-				for i, line in enumerate(input_file): # set all cPanel accounts to default main ip
-					setip="/usr/local/cpanel/bin/setsiteip -u %s %s" % (i, mainip)
-					print(setip)
-				#subprocess.call(setip, shell=True)
-			print("\nAll user accounts are now using the shared IP.")
-		except IOError:
-			print("\nFailed, something went from.")
-	#with open('/etc/trueuserdomain') as input_file:
-	#	for i, line in enumerate(input_file): # set all dedicated ip cPanel accounts to proper IP
-	#		setip="/usr/local/cpanel/bin/setsiteip -u %s %s" % (i, mainip)
-	#		print(setip)
-	#		subprocess.call(setip, shell=True)
+#whmapi1 setsiteip ip=192.168.4.10 user=username domain=domainname
+	mainip=open('/var/cpanel/mainuip', 'r')
+	with open('/etc/trueuserdomain') as input_file:
+		for i in input_file: # set all cPanel accounts to default main ip
+			setip="whmapi1 setsiteip ip=%s user= %s" % (i, mainip)
+			print(setip)
+			#subprocess.call(setip, shell=True)
+		print("\nAll user accounts are now using the shared IP.")
+		
+	with open('/etc/domainips') as input_file:
+		for i in input_file: # set all dedicated ip cPanel accounts to proper IP
+			input_file.split(':')
+			setip="whmapi1 setsiteip ip=%s domain= %s" % (input_file[0], input_file[1])
+			print(setip)
+			#subprocess.call(setip, shell=True)
+
+if __name__ == "__main__":
+    main()
