eec0259d681bd788db46a902449c5d3bc09b6bff
Dave <dhurley@singlehop.com>
2016-09-30 11:27:07 -0400

    Update sshkey.py

--- sshkey.py
+++ sshkey.py
@@ -4,20 +4,24 @@
 import subprocess
 from optparse import OptionParser
 
-parser = OptionParser()
+def main():
+	parser = OptionParser()
 
-parser.add_option("-s", "--source", dest="sourceip",
-                help="set the source ip address.")
+	parser.add_option("-s", "--source", dest="sourceip",
+					help="set the source ip address.")
 				  
-parser.add_option("-p", "--port", dest="sourceport", default="22"
-                help="set port, defaults to 22 if not set")
+	parser.add_option("-p", "--port", dest="sourceport", default="22"
+					help="set port, defaults to 22 if not set")
 				  
-parser.add_option("-u", "--user", dest="user", default="root"				  
-		help="set username to be ssh keyed, defaults to root")
+	parser.add_option("-u", "--user", dest="user", default="root"				  
+			help="set username to be ssh keyed, defaults to root")
 				  
-(options, args) = parser.parse_args()
+	(options, args) = parser.parse_args()
 
-if (options.sourceip is None):
-	print("You will need to set the source IP");
-else 
-	subprocess.call([genkey], shell=True)
+	if (options.sourceip is None):
+		print("You will need to set the source IP");
+	else 
+		subprocess.call([genkey], shell=True)
+		
+if __name__ == "__main__":
+    main()
