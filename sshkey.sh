2588f78c43a5ab5d3d78bae9199c8a1d89690019
Dave <dhurley@singlehop.com>
2016-09-30 10:57:34 -0400

    Update sshkey.sh

--- sshkey.sh
+++ sshkey.sh
@@ -10,22 +10,19 @@ REQUIRED OPTIONS:
       set the source ip address.
 	  
 	-u <root>
-      user you want the public key on
+      user you want the public key on, defaults to root
 	  
 OPTIONS:
 	-p <2222>
       set the source port, defaults to 22.
 	
-Ex: ./sshkey.sh -s 127.0.0.1 -u root -p 2222
+Ex: ./sshkey.sh -s 127.0.0.1 -u frank -p 2222
 "
 	exit 1
 }
 
 setup_sshkey() { # generate keys and push to source
-	ssh-keygen -t rsa
-	ssh-copy-id -p $sourceport $user@$sourceip
-	sshkey=0
-	echo; echo
+	cat ~/.ssh/id_rsa.pub | ssh $user@$sourceip -p $sourceport "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys"
 }
 
 while getopts ":s:p:u:k:h" opt; do
@@ -52,9 +49,12 @@ while getopts ":s:p:u:k:h" opt; do
 done
 
 if [[ $# -eq 0 || -z $sourceip ]]; then MENU; fi  # check for existence of required var
-if [ -z $sourceport ]; then sourceport=22; fi # apply port 22 if none is set
-
-setup_sshkey
-printf "\nTesting SSH Keys. \n"
-ssh $user@$sourceip -p $sourceport
-printf "\nYou are now using SSH keys!\n"
+if [ -z "$sourceport" ]; then sourceport=22; fi # apply port 22 if none is set
+if [ -z "$user" ]; then user="root"; fi
+if [ -e /etc/redhat-release ]; then
+	setup_sshkey
+	printf "\nTesting SSH Keys. \n"
+	ssh $user@$sourceip -p $sourceport
+	printf "\nYou are now using SSH keys!\n"
+	exit
+fi
