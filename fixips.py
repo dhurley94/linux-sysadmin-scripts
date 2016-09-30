c48ee583dd7c9f0769e8fbe15b410209e4f5122f
Dave <dhurley@singlehop.com>
2016-09-30 19:27:50 -0400

    Update fixips.py

--- fixips.py
+++ fixips.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
 import subprocess
-main(): # whmapi1 setsiteip ip=192.168.4.10 user=username domain=domainname
-	mainip=open('/var/cpanel/mainuip', 'r')
-	with open('/etc/trueuserdomain') as input_file:
-		for i in input_file: # set all cPanel accounts to default main ip
-			setip="whmapi1 setsiteip ip=%s user= %s" % (i, mainip)
-			print(setip)
-		
-	with open('/etc/domainips') as input_file:
-		for i in input_file: # set all dedicated ip cPanel accounts to proper IP
-			input_file.split(':')
-			input_file[0].translate(':','')
-			setip="whmapi1 setsiteip ip=%s domain= %s" % (input_file[0], input_file[1])
-			print(setip)
-
+def main():
+        f=open('/var/cpanel/mainip', 'r')
+        mainip=f.read()
+        with open('/etc/trueuserdomains') as input_file:
+                for i in input_file: # set all cPanel accounts to default main ip
+                        i=i.split()
+                        setip="whmapi1 setsiteip ip=%s user=%s" % (mainip, i[1])
+                        print(setip)
+                        subprocess.call(setip, shell=True)
+        with open('/etc/domainips') as input_file:
+                next(input_file)
+                for i in input_file: # set all dedicated ip cPanel accounts to proper IP
+                        i=i.split()
+                        print(i)
+                        i[0]=i[0].replace(':','')
+                        i[1]=i[1].replace(' ','')
+                        setip="whmapi1 setsiteip ip=%s domain= %s" % (i[0], i[1])
+                        subprocess.call(setip, shell=True)
 if __name__ == "__main__":
     main()
