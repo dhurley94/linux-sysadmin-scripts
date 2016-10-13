3dca7d948a0d3a87bcc7468a290db9a0222dfe32
Dave <dhurley@singlehop.com>
2016-10-13 04:43:05 -0400

    Update sshkey.py

--- sshkey.py
+++ sshkey.py
@@ -7,15 +7,13 @@ def main():
         parser.add_option("-s", "--source", dest="sourceip", type=str,
                                         help="set the source ip address.")                                
         parser.add_option("-p", "--port", dest="sourceport", default="22",
-                                        help="set port, defaults to 22 if not set")                               
-        parser.add_option("-u", "--user", dest="user", default="root",                            
-                                        help="set username to be ssh keyed, defaults to root")                            
+                                        help="set port, defaults to 22 if not set")                                                         
         (options, args) = parser.parse_args()
         if (options.sourceip is None):
                subprocess.call("python sshkey.py --help", shell=True)
         else:
                genkey="ssh-keygen -t rsa"
-               sendkey="ssh-copy-id %s@%s -p %s" % (options.user, options.sourceip, options.sourceport)
+               sendkey="ssh-copy-id -p %s %s" % (options.sourceport, options.sourceip)
                subprocess.call(genkey, shell=True)
                subprocess.call(sendkey, shell=True)
 if __name__ == "__main__":
