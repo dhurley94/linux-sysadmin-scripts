3fb64eb58450bbf176514e27de84ddc6bdf50954
Dave <dhurley@singlehop.com>
2016-10-27 01:41:11 -0400

    Update environmentmatch.py

--- environmentmatch.py
+++ environmentmatch.py
@@ -1,8 +1,39 @@
-#doesn't work
+# in progress, do not use
+# script is becoming overly complex for such a simple task.
+# possibly remove version checking
 #!/usr/bin/env python
 import subprocess
 from optparse import OptionParser
 import os
+import MySQLdb
+
+def getphpversion(server, sourceip, sourceport):
+	# may not be needed as php version isn't extremely important atm
+	try :
+		if (server == "dst"):
+			dstphp = subprocess.check_output(["php", "-v"])
+			return True
+		elif (server == "src":
+			# remote ssh cmd
+			# https://github.com/paramiko/paramiko
+			return True
+	except:
+		return False
+
+def getsqlversion(server, dbpass, sourceip, sourceport): # requires remote root mysql user for src
+	# add try / except
+	if (server == "dst"):
+		db = MySQLdb.connect(host="localhost",
+						user="root",
+						passwd=dbpass)
+	elif (server == "src":
+		db = MySQLdb.connect(host=sourceip,
+						user="root",
+						passwd=dbpass)
+		cur=db.cursor()
+	version = cur.execute("SELECT VERSION()")
+	cur.close()
+	return version
 
 def match(sourceip, sourceport):
 	mktar="tar -czf environment.tar.gz /usr/local/lib/php.ini /etc/my.cnf /var/cpanel/easy/apache/profile/_la st_success.yaml /var/cpanel/easy/apache/profile/_main.yaml"
@@ -19,7 +50,9 @@ def main():
     parser.add_option("-s", "--source", dest="sourceip", type=str,
                                         help="set the source ip address.")                                
     parser.add_option("-p", "--port", dest="sourceport", default="22",
-                                        help="set port, defaults to 22 if not set")							
+                                        help="set port, defaults to 22 if not set")	
+	parser.add_option("-r", "--dbpass", dest="dbpass", type=str,
+                                        help="set mysql root user pass, if set.")
     (options, args) = parser.parse_args()
     if (options.sourceip is None):
 		subprocess.call("python environmentmatch.py --help", shell=True)
