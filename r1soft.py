17f85c835a8881c496342689bb22d68b8728d9b4
Dave <dhurley@singlehop.com>
2016-10-26 22:31:51 -0400

    Update r1soft.py

--- r1soft.py
+++ r1soft.py
@@ -1,47 +1,46 @@
 #!/usr/bin/env python
 import subprocess
-from pathlib import Path
+import os
 from optparse import OptionParser
 
 def install(key):
-	intall = "yum -y install serverbackup-enterprise-agent"
-	addkey="serverbackup-setup --get-key=http://%s"  % key
-	getmod="serverbackup-setup --get-module"
-	cdprestart="service cdp-agent restart"
-	subprocess.call(install, shell=True)
-	subprocess.call(addkey, shell=True)
-	subprocess.call(getmod, shell=True)
-	subprocess.call(cdprestart, shell=True)
+    install = "yum -y install serverbackup-enterprise-agent"
+    addkey="serverbackup-setup --get-key=http://%s"  % (key)
+    getmod="serverbackup-setup --get-module"
+    cdprestart="service cdp-agent restart"
+    subprocess.call(install, shell=True)
+    subprocess.call(addkey, shell=True)
+    subprocess.call(getmod, shell=True)
+    subprocess.call(cdprestart, shell=True)
 
 def main():
-	usage = "usage: python %prog [options] arg"
-    parser = OptionParser(usage) 
+    usage = "usage: python %prog [options] arg"
+    parser = OptionParser(usage)
     parser.add_option("-k", "--keys", dest="key", type=str,
-                                        help="set r1soft server ip")                                
+                                        help="set r1soft server ip")
     parser.add_option("-r", "--repo", dest="createrepo", default="1",
-                                        help="create repo, set to 1/0. defaults to 1")								
+                                        help="create repo, set to 1/0. defaults to 1")
     (options, args) = parser.parse_args()
-	
-	if (options.key is None):
-		printf("The R1soft server is not set.
-				Please set a server IP.
-				")
-		subprocess.call("python r1soft.py --help", shell=True)
-	else:
-		repo="
+
+    if (options.key is None):
+        print("The R1soft server is not set.")
+        subprocess.call("python r1soft.py --help", shell=True)
+    else:
+        repo="""
 [r1soft]
 name=R1Soft Repository Server
 baseurl=http://repo.r1soft.com/yum/stable/$basearch/
 enabled=1
-gpgcheck=0"
-		r1_repo="/etc/yumrepos.d/r1soft.repo"
-		ins="echo %s > %s" % (repo, r1_repo)
-		subprocess.call(ins, shell=True)
-		if (r1_repo.is_file()):
-			printf("Installing R1soft")
-			install(key)
-		else:
-			printf("R1soft repo does not exist.")
-			
+gpgcheck=0"""
+        r1_repo="/etc/yum.repos.d/r1soft.repo"
+        r1soft=open(r1_repo, 'w+')
+        r1soft.write(repo)
+        r1soft.close()
+        if (os.path.isfile(r1_repo)):
+             print("Installing R1soft")
+             install(options.key)
+        else:
+             print("R1soft repo does not exist.")
+
 if __name__ == "__main__":
         main()
