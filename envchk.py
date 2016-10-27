c6bdab26f766d48ac812d1fea025cb69fa573aed
Dave <dhurley@singlehop.com>
2016-10-27 01:52:50 -0400

    Update envchk.py

--- envchk.py
+++ envchk.py
@@ -4,14 +4,17 @@
 import subprocess
 from optparse import OptionParser
 import os
+import logging
 
 def match(sourceip, sourceport):
 	mktar="tar -czf environment.tar.gz /usr/local/lib/php.ini /etc/my.cnf /var/cpanel/easy/apache/profile/_la st_success.yaml /var/cpanel/easy/apache/profile/_main.yaml"
 	grabballs="rsync -avze 'ssh -p %s' %s:/root/environment.tar.gz /root" % (sourceport, sourceip)
 	if (os.path.isfile("/root/environment.tar.gz")):
+		logging.info("tarball has successfully been downloaded to dst.")
 		return True
 
 def main():
+    logging.basicConfig(filename='envchk.log',level=logging.DEBUG)
     usage = "usage: python %prog [options] arg"
     parser = OptionParser(usage) 
     parser.add_option("-s", "--source", dest="sourceip", type=str,
@@ -24,11 +27,15 @@ def main():
     else:
 		match(options.sourceip, options.sourceport)
 		if (match()):
+			logging.info("uncompressing environment configs.")
 			subprocess.call("tar -xf environment.tar.gz -C /", shell=True)
+			logging.info("building easyapache.")
 			subprocess.call("/scripts/easyapache --build", shell=True)
+			logging.info("needful has been completed.")
 			print("\n2ez4\n2sbz\n")
 		else:
-			print("\nSomething went wrong.")
+			print("\nFailed to create tarball.")
+			logging.info("unable to needful.")
 		
 if __name__ == "__main__":
         main()
