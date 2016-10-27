55f7597194d4d092922b811a0b59e80184f988bd
Dave <dhurley@singlehop.com>
2016-10-27 02:05:55 -0400

    Update sshkey.py

--- sshkey.py
+++ sshkey.py
@@ -13,8 +13,12 @@ def main():
                subprocess.call("python sshkey.py --help", shell=True)
         else:
                genkey="ssh-keygen -t rsa"
-               sendkey="ssh-copy-id -p %s %s" % (options.sourceport, options.sourceip)
+               setperm=""ssh root@%s -p %s "mkdir ~/.ssh && chmod 700 ~/.ssh && chmod 600 .ssh/authorized_keys""" % (options.sourceip, options.sourceport)
+               sendkey=""cat ~/.ssh/id_rsa.pub | ssh root@%s -p %s "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys""" % (options.sourceip, options.sourceport)
+               #sendkey="ssh-copy-id -p %s %s" % (options.sourceport, options.sourceip)
+               subprocess.call(setperm, shell=True)
                subprocess.call(genkey, shell=True)
                subprocess.call(sendkey, shell=True)
+               access="ssh root@%s -p %s " % (options.sourceip, options.sourceport)
 if __name__ == "__main__":
         main()
