d578c0d9e2fb7ccd8148e8889e349f2ccdc2b2f8
Dave <dhurley@singlehop.com>
2016-10-26 23:34:23 -0400

    Update ipswap.py

--- ipswap.py
+++ ipswap.py
@@ -1,27 +1,40 @@
+#doesn't work
+
 #!/usr/bin/env python
 import subprocess
 from optparse import OptionParser
+import os
 
-def sshkeys(){
+def removehw():
+	ifcfg = open("/etc/sysconfig/network-scripts/ifcfg-eth0","r")
+	lines = ifcfg.readlines()
+	ifcfg = open("/etc/sysconfig/network-scripts/ifcfg-eth0","w")
+	for line in lines:
+		if line!="^HWADDR" or line!="^UUID": # doesn't work
+			ifcfg.write(line)	
+
+def sshkeys(sourceport, sourceip):
 	genkey="ssh-keygen -t rsa"
-	sendkey="ssh-copy-id -p %s %s" % (options.sourceport, options.sourceip)
-	subprocess.call(genkey, shell=True)
-	subprocess.call(sendkey, shell=True)
-}
+    sendkey="ssh-copy-id -p %s %s" % (sourceport, sourceip)
+    subprocess.call(genkey, shell=True)
+    subprocess.call(sendkey, shell=True)
 
-def fixips() {
-	f=open('/var/cpanel/mainip', 'r')
-        mainip=f.read()
-        print("setting all cPanel accounts to default shared ip.\n")
-        with open('/etc/trueuserdomains') as input_file:
-                for i in input_file: # set all cPanel accounts to default main ip
+def fixips(sourceport, sourceip):
+	grabips="rsync -ave 'ssh -p %s' %s:/etc/domainips /etc/domainips-src" %s (sourceip, sourceport)
+	subprocess.call(grabips, shell=True)
+	if (os.path.isfile("/etc/domainips-src")):
+		f=open('/var/cpanel/mainip', 'r')
+			mainip=f.read()
+			print("setting all cPanel accounts to default shared ip.\n")
+			with open('/etc/trueuserdomains') as input_file:
+				for i in input_file: # set all cPanel accounts to default main ip
                         i=i.split()
                         print("\n%s -> %s" % (i[1], mainip))
                         setip="whmapi1 setsiteip ip=%s user=%s" % (mainip, i[1])
                         subprocess.call(setip, shell=True)
-        print("\n\n\n")
-        print("setting dedicated ip cPanel accounts to correct ip.\n")
-        with open('/etc/domainips-src') as input_file:
+			print("\n\n\n")
+			print("setting dedicated ip cPanel accounts to correct ip.\n")
+			with open('/etc/domainips-src') as input_file:
                 next(input_file)
                 for i in input_file: # set all dedicated ip cPanel accounts to proper IP
                         i=i.split()
@@ -30,7 +43,6 @@ def fixips() {
                         print("\n%s -> %s" % (i[1], i[0]))
                         setip="whmapi1 setsiteip ip=%s domain=%s" % (i[0], i[1])
                         subprocess.call(setip, shell=True)
-}
 
 def main():
         usage = "usage: python %prog [options] arg"
@@ -45,12 +57,7 @@ def main():
         if (options.sourceip is None):
 			subprocess.call("python sshkey.py --help", shell=True)
         else:
-			ifcfg = open("/etc/sysconfig/network-scripts/ifcfg-eth0","r")
-			lines = ifcfg.readlines()
-			ifcfg = open("/etc/sysconfig/network-scripts/ifcfg-eth0","w")
-			for line in lines:
-				if line!="^HWADDR" or line!="^UUID":
-					ifcfg.write(line)
+			# do a thing
 					
 if __name__ == "__main__":
         main()
