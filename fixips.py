50cebc620a73c72fbb769a48b39a6ebcef2498d0
Dave <dhurley@singlehop.com>
2016-11-02 18:21:16 -0400

    Update fixips.py

--- fixips.py
+++ fixips.py
@@ -21,34 +21,34 @@ def main():
                     grabips = "rsync -ave 'ssh -p %s' %s:/etc/domainips /etc/domainips-src" % (options.sourceport, options.sourceip)
                     subprocess.call(grabips, shell=True)
                 except Exception:
-                    print ("\nrsync failed\n")
-                else:
-                    f=open('/var/cpanel/mainip', 'r')
-                    mainip=f.read()
-                    try:
-                        input_file=open('/etc/trueuserdomains','r')
-                        for i in input_file.readline():  # set all cPanel accounts to default main ip
-                            i = i.split()
-                            print("\n%s -> %s\n" % (i[1], mainip))
-                            setip = "whmapi1 setsiteip ip=%s user=%s" % (mainip, i[1])
-                            subprocess.call(setip, shell=True)
-                            print("setting all cPanel accounts to default shared ip.\n")
-                    finally:
-                        input_file.close()
-                    try:
-                        input_file=open('/etc/domainips-src','r')
-                        next(input_file)
-                        for i in input_file:  # set all dedicated ip cPanel accounts to proper IP
-                            i = i.split()
-                            i[0] = i[0].replace(':', '')
-                            i[1] = i[1].replace(' ', '')
-                            print("\n%s -> %s\n" % (i[1], i[0]))
-                            setip = "whmapi1 setsiteip ip=%s domain=%s" % (i[0], i[1])
-                            subprocess.call(setip, shell=True)
-                    finally:
-                        input_file.close()
-                finally:
-                    print("\n2ez42sbz\n")
+                    print ("\nrsync failed\nplease retry and manually move the /etc/domainips file to /etc/domainips-src on destination system\n")
                     quit()
+        if (os.path.isfile("/etc/domainips-src")):
+            f=open('/var/cpanel/mainip', 'r')
+            mainip=f.read()
+            try:
+                input_file=open('/etc/trueuserdomains','r')
+                for i in input_file.readline():  # set all cPanel accounts to default main ip
+                    i = i.split()
+                    print("\n%s -> %s\n" % (i[1], mainip))
+                    setip = "whmapi1 setsiteip ip=%s user=%s" % (mainip, i[1])
+                    subprocess.call(setip, shell=True)
+                    print("setting all cPanel accounts to default shared ip.\n")
+            finally:
+                    input_file.close()
+            try:
+                input_file=open('/etc/domainips-src','r')
+                next(input_file)
+                for i in input_file:  # set all dedicated ip cPanel accounts to proper IP
+                    i = i.split()
+                    i[0] = i[0].replace(':', '')
+                    i[1] = i[1].replace(' ', '')
+                    print("\n%s -> %s\n" % (i[1], i[0]))
+                    setip = "whmapi1 setsiteip ip=%s domain=%s" % (i[0], i[1])
+                    subprocess.call(setip, shell=True)
+            finally:
+                    input_file.close()
+            print("\n2ez42sbz\n")
+            quit()
 if __name__ == "__main__":
     main()
