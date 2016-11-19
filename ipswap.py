238a34f2b4270f8573a757d255921c5b271c96ee
Dave <dhurley@singlehop.com>
2016-11-19 18:33:41 -0500

    Update ipswap.py

--- ipswap.py
+++ ipswap.py
@@ -2,26 +2,29 @@
 import subprocess
 from optparse import OptionParser
 import os
-import logging
-#import json
+
+def exists_remote(host, path, port):
+    status = subprocess.call(
+        ['ssh', host, '-p', port, 'test -f {}'.format(pipes.quote(path))])
+    if status == 0:
+        return True
+    if status == 1:
+        return False
+    raise Exception('SSH failed')
 
 def backup(server): # backup all networking related files, iterate w/ src / dst
-	logging.info('Creating tarballs for %s', server)
 	tarballs="tar -czf /root/%s-network.tar.gz /etc/sysconfig/network-scripts/ifcfg-eth0 /etc/sysconfig/network /etc/ips /var/cpanel/mainip /etc/hosts" % (server)
 	if (os.path.isfile("/root/%s-network.tar.gz", server)):
-		logging.info('tarballs for %s have been created.', server)
-		return True
+		print "\nFile $s-network.tar.gz already exists.\n" % (server)
 	else:
-		logging.warning('tarballs for %s have failed. exiting now.', server)
-		return False
+		subprocess.call(tarballs, shell=True)
 
-def removehw(server): # rewrites ifcfg w/o hwaddr/uuid
+def removehw(): # rewrites ifcfg w/o hwaddr/uuid
 	ifcfg = open("/etc/sysconfig/network-scripts/ifcfg-eth0","r+")
 	lines = ifcfg.readlines()
 	for line in lines:
 		if ("HWADDR" not in line or "UUID" not in line):
 			ifcfg.write(line)
-		logging.info('MAC has been removed from %s ifcfg.', server)
 	ifcfg.close()
 
 def sshkeys(sourceport, sourceip): # create and apply sshkeys
@@ -31,53 +34,32 @@ def sshkeys(sourceport, sourceip): # create and apply sshkeys
 	subprocess.call(sendkey, shell=True)
 	# find a way to check if this was successful w/o manual connection
 
-def fixips(sourceport, sourceip): # replaces cPanel ips on new server with old server's
-	if (os.path.isfile("/etc/domainips-src")):
-		f=open('/var/cpanel/mainip', 'r')
-		mainip=f.read()
-		print("setting all cPanel accounts to default shared ip.\n")
-		with open('/etc/trueuserdomains') as input_file:
-			for i in input_file: # set all cPanel accounts to default main ip
-                       		i=i.split()
-                       		print("\n%s -> %s\n" % (i[1], mainip))
-                       		setip="whmapi1 setsiteip ip=%s user=%s" % (mainip, i[1])
-                       		subprocess.call(setip, shell=True)
-				logging.info("\n%s -> %s\n", i[1], mainip)
-		with open('/etc/domainips-src') as input_file:
-                	next(input_file)
-                	for i in input_file: # set all dedicated ip cPanel accounts to proper IP
-                        	i=i.split()
-                        	i[0]=i[0].replace(':','')
-                        	i[1]=i[1].replace(' ','')
-                        	print("\n%s -> %s\n" % (i[1], i[0]))
-                        	setip="whmapi1 setsiteip ip=%s domain=%s" % (i[0], i[1])
-                        	subprocess.call(setip, shell=True)
-				logging.info("\n%s -> %s\n", i[1], i[0]) # chk json and verify success before logging
-	# take json output and parse to log file / verify success
-
 def main():
-    logging.basicConfig(filename='ipswap.log',level=logging.DEBUG)
     usage = "usage: python %prog [options] arg"
     parser = OptionParser(usage) 
     parser.add_option("-s", "--source", dest="sourceip", type=str,
                                         help="set the source ip address.")                                
     parser.add_option("-p", "--port", dest="sourceport", default="22",
                                         help="set port, defaults to 22 if not set")
-    parser.add_option("-i", "--ipfix", dest="fixipVar", default="0",
-                                        help="set bit to run fixips.py after successful IP migration") 										
+    				
     (options, args) = parser.parse_args()
     if (options.sourceip is None):
-	subprocess.call("python sshkey.py --help", shell=True)
+        subprocess.call("python sshkey.py --help", shell=True)
     else:
-	grabips="rsync -ave 'ssh -p %s' %s:/etc/domainips /etc/domainips-src" % (sourceport, sourceip)
-	subprocess.call(grabips, shell=True)
-	# remove existance of old network tarballs from both systems
+        sshkey(options.sourceport, options.sourceip)
+        backup("dst-dst")
+        removehw()
+        backup("dst-src")
+        subprocess.call('ssh', options.sourceip, '-p', options.sourceport, "tar -czf /root/src-src-network.tar.gz /etc/sysconfig/network-scripts/ifcfg-eth0 /etc/sysconfig/network /etc/ips /var/cpanel/mainip /etc/hosts")
+        if exists_remote(options.sourceip, 'src-src-network.tar.gz', options.port)
+            subprocess.call('tar -xf src-src-network.tar.gz -C /')
+            removehw()
+          
 	# sshkey
 	# remove hwaddr/uuid from dst, create tarball, send to src
 	# create tarball on src, send to dst, remove hwaddr/uuid
 	# perform network conf swap
 	# verify files, restart networking, reload ipaliases, rebuildhttpdconf, etc.
-	# user input on success, if y then fixips
 		
 if __name__ == "__main__":
         main()
