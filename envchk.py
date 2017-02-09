3950d14dd3eafd62e0a3b9386412631dc4f7e672
Dave <dhurley@singlehop.com>
2017-02-08 20:03:52 -0500

    Update envchk.py

--- envchk.py
+++ envchk.py
@@ -1,36 +1,18 @@
-# in progress, do not use
 #!/usr/bin/env python
 import subprocess
 from optparse import OptionParser
 import os
 
-def getphpversion():
-	return subprocess.check_output("whmapi1 php_get_handlers")
-def getsqlversion():
-	return subprocess.check_output("whmapi1 current_mysql_version")
-
-def versionlists(isini): # only checks dst
-	if (isini): # before matching
-		versionlist.append(1, "php", getphpversion())
-		versionlist.append(1, "sql", getsqlversion())
-	else: # after matching
-		versionlist.append(0, "php", getphpversion())
-		versionlist.append(0, "sql", getsqlversion())
-def match(sourceip, sourceport):
-	mktar="tar -czf environment.tar.gz /usr/local/lib/php.ini /etc/my.cnf /var/cpanel/easy/apache/profile/_la st_success.yaml /var/cpanel/easy/apache/profile/_main.yaml"
+def createEnvironmentBalls(sourceip, sourceport):
+	mktar="ssh root@%s -p %s 'tar -czf /root/environment.tar.gz /usr/local/lib/php.ini /etc/my.cnf /var/cpanel/easy/apache/profile/_la st_success.yaml /var/cpanel/easy/apache/profile/_main.yaml'" % (sourceip, sourceport)
 	grabballs="rsync -ave 'ssh -p %s' %s:/root/environment.tar.gz /root" % (sourceport, sourceip)
-	try: 
-		subprocess.call(grabballs, shell=True)
-	except Exception:
-		print("importing the tarballs failed.")
-		return False
-	else:
-		subprocess.call("tar -xf environment.tar.gz -C /", shell=True)
-		subprocess.call("/scripts/easyapache --build", shell=True)
-		return True
+	subprocess.call(mktar, shell=True)
+	subprocess.call(grabballs, shell=True)
+	if (!os.path.isfile('/root/environment.tar.gz')):
+		print("Something went wrong.")
+		quit()
 
 def main():
-    versionlist = list()
     usage = "usage: python %prog [options] arg"
     parser = OptionParser(usage) 
     parser.add_option("-s", "--source", dest="sourceip", type=str,
@@ -40,13 +22,11 @@ def main():
     (options, args) = parser.parse_args()
     if (options.sourceip is None):
 		subprocess.call("python environmentmatch.py --help", shell=True)
-    else:
-		versionlists(1)
-		for i in versionlist:
-			print i
-		match(options.sourceip, options.sourceport)		
-		versionlists(0)
-		for i in versionlist:
-			print i
+		quit()
+	else:
+		if (os.path.isfile('/root/environment.tar.gz')):
+			untarball="tar -xf /root/environment.tar.gz -C /"
+			subprocess.call(untarball, shell=True)
+			subprocess.call("/scripts/easyapache --default", shell=True)
 if __name__ == "__main__":
         main()
