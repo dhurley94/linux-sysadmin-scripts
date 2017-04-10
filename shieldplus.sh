bcc65a57a94ccc848f92cf634a0950f7caeb1f9f
Dave <dhurley@singlehop.com>
2017-04-10 02:14:50 -0400

    Create shieldplus.sh

---
+++ shieldplus.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd /usr/src
+rm -fv csf.tgz
+wget https://download.configserver.com/csf.tgz
+tar -xzf csf.tgz
+cd csf
+if [ -d "/var/cpanel" ]; then
+	sh install.cpanel.sh
+else
+	sh install.sh
+fi
+
+curl http://wiki.cryptohost.io/sh_noc_ips > /etc/csf/csf.allow
+curl http://wiki.cryptohost.io/sh_noc_ips > /etc/csf/csf.ignore
+
+csf -r
+
+cd /usr/src
+rm -fv maldetect-current.tar.gz
+wget http://www.rfxn.com/downloads/maldetect-current.tar.gz
+tar -xvf maldetect-current.tar.gz
+cd maldet-*
+sh install.sh
