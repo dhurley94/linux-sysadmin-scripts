bcac17991701cbb0bdad05d8d6f04734b0d77818
Dave <dhurley@singlehop.com>
2017-04-27 12:20:17 -0400

    Update shiledplus.sh

--- shiledplus.sh
+++ shiledplus.sh
@@ -10,3 +10,10 @@ if [[ -e /var/cpanel ]]; then
 else
     sh install.sh
 done
+
+wget http://www.rfxn.com/downloads/maldetect-current.tar.gz
+tar -xvzf maldetect-current.tar.gz
+cd maldet-*
+sh install.sh
+
+/usr/sbin/useradd -p `date +%s | sha256sum | base64 | head -c 16; echo;` singlehop
