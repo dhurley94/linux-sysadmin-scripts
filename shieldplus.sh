9a05764617ddd48b2d3f9f18278ab56381186882
Dave <dhurley@singlehop.com>
2017-08-15 11:15:09 -0400

    Update shieldplus.sh

--- shieldplus.sh
+++ shieldplus.sh
@@ -1,28 +1,42 @@
 #!/bin/bash
 
-cd /usr/src
-rm -fv csf.tgz
-wget https://download.configserver.com/csf.tgz
-tar -xzf csf.tgz
-cd csf
-if [[ -e /var/cpanel ]]; then
-    sh install.cpanel.sh
-else
-    sh install.sh
+if [[ ! -e /usr/sbin/csf ]]; then
+    cd /usr/src
+    rm -fv csf.tgz
+    wget https://download.configserver.com/csf.tgz
+    tar -xzf csf.tgz
+    cd csf
+    if [[ -e /var/cpanel ]]; then
+        sh install.cpanel.sh
+        curl https://raw.githubusercontent.com/dhurley94/ip-swap/master/csf.conf > /etc/csf/csf.conf
+    else
+        sh install.sh
+        curl https://raw.githubusercontent.com/dhurley94/ip-swap/master/csf.conf > /etc/csf/csf.conf
+    fi
+else 
+    echo "CSF already installed."
 fi
 
-curl https://raw.githubusercontent.com/dhurley94/ip-swap/master/ships > /etc/csf/csf.allow
-curl https://raw.githubusercontent.com/dhurley94/ip-swap/master/ships > /etc/csf/csf.ignore
-curl https://raw.githubusercontent.com/dhurley94/ip-swap/master/csf.conf > /etc/csf/csf.conf
+curl https://raw.githubusercontent.com/dhurley94/ip-swap/master/ships >> /etc/csf/csf.allow
+curl https://raw.githubusercontent.com/dhurley94/ip-swap/master/ships >> /etc/csf/csf.ignore
 
-rm -fv maldetect-current.tar.gz
-wget http://www.rfxn.com/downloads/maldetect-current.tar.gz
-tar -xvzf maldetect-current.tar.gz
-cd maldetect-*
-sh install.sh
+if [[ ! -e /usr/local/sbin/maldet ]]; then
+    rm -fv maldetect-current.tar.gz
+    wget http://www.rfxn.com/downloads/maldetect-current.tar.gz
+    tar -xvzf maldetect-current.tar.gz
+    cd maldetect-*
+    sh install.sh
+else
+    echo "Maldet already installed."
+    if [[ -e /var/cpanel ]]; then
+        /usr/local/cpanel/3rdparty/bin/freshclam
+    fi
+fi
 
 sed -i '/Port 22/c\Port 2222' /etc/ssh/sshd_config
 sed -i '/PermitRootLogin/c\PermitRootLogin no' /etc/ssh/sshd_config
+
+service sshd restart
 
 PASS=`openssl rand -base64 12`
 
