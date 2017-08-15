dd4574609536c00af89fe6aa2a4889dbab842258
Dave <dhurley@singlehop.com>
2017-08-15 11:19:45 -0400

    Update shieldplus.sh

--- shieldplus.sh
+++ shieldplus.sh
@@ -3,38 +3,39 @@
 if [[ ! -e /usr/sbin/csf ]]; then
     cd /usr/src
     rm -fv csf.tgz
-    wget https://download.configserver.com/csf.tgz
+    /usr/bin/wget https://download.configserver.com/csf.tgz
     tar -xzf csf.tgz
     cd csf
     if [[ -e /var/cpanel ]]; then
         sh install.cpanel.sh
-        curl https://raw.githubusercontent.com/dhurley94/ip-swap/master/csf.conf > /etc/csf/csf.conf
+        /usr/bin/curl https://raw.githubusercontent.com/dhurley94/ip-swap/master/csf.conf > /etc/csf/csf.conf
     else
         sh install.sh
-        curl https://raw.githubusercontent.com/dhurley94/ip-swap/master/csf.conf > /etc/csf/csf.conf
+        /usr/bin/curl https://raw.githubusercontent.com/dhurley94/ip-swap/master/csf.conf > /etc/csf/csf.conf
     fi
 else 
     echo "CSF already installed."
 fi
 
-curl https://raw.githubusercontent.com/dhurley94/ip-swap/master/ships >> /etc/csf/csf.allow
-curl https://raw.githubusercontent.com/dhurley94/ip-swap/master/ships >> /etc/csf/csf.ignore
+/usr/bin/curl https://raw.githubusercontent.com/dhurley94/ip-swap/master/ships >> /etc/csf/csf.allow
+/usr/bin/curl https://raw.githubusercontent.com/dhurley94/ip-swap/master/ships >> /etc/csf/csf.ignore
 
 if [[ ! -e /usr/local/sbin/maldet ]]; then
     rm -fv maldetect-current.tar.gz
-    wget http://www.rfxn.com/downloads/maldetect-current.tar.gz
+    /usr/bin/wget http://www.rfxn.com/downloads/maldetect-current.tar.gz
     tar -xvzf maldetect-current.tar.gz
     cd maldetect-*
     sh install.sh
 else
     echo "Maldet already installed."
+    /usr/local/sbin/maldet -u
     if [[ -e /var/cpanel ]]; then
         /usr/local/cpanel/3rdparty/bin/freshclam
     fi
 fi
 
 sed -i '/Port 22/c\Port 2222' /etc/ssh/sshd_config
-sed -i '/PermitRootLogin/c\PermitRootLogin no' /etc/ssh/sshd_config
+sed -i '/PermitRootLogin yes/c\PermitRootLogin no' /etc/ssh/sshd_config
 
 service sshd restart
 
